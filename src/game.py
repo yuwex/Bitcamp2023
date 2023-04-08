686c6451f1efe46b97b5a5b60456b650114fefee
yuwe <25470828+yuwex@users.noreply.github.com>
2023-04-08 18:06:00 -0400

    Refactor game
    Move everything into one class
    Re-draw screen correctly
    other things too

--- src/game.py
+++ src/game.py
@@ -3,31 +3,40 @@ A class for handling game mechanics
 """
 import pygame
 import time
-pygame.init()
-#params for the game screen
-width = 600
-height = 400
-display = pygame.display.set_mode((width, height))
-#colors for the game beta
-black = (0,0,0)
-yellow = (255,255,102)
-red = (213,50,80)
-green = (0,255,0)
-blue =(50,153,213)
-#style for the game screen
-scoreStyle = pygame.font.SysFont('Treasuremap.ttf', 35)
-pygame.display.set_caption('Email Pirates')
 
-#displaying the score
-def yourScore(score):
-        value = scoreStyle.render('Score: ' + str(score), True, yellow)
-        display.blit(value, [0,0])
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 102)
+RED = (213, 50, 80)
+GREEN = (0, 255, 0)
+BLUE = (50, 153, 213)
 
-#makes the moving background
-def background(left, color):
-      pygame.draw.rect(display, color, [left, 100, 10, 10])
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
 
 class Game:
+
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('Email Pirates')
+
+        self.display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+        self.start()
+    
+    def start(self):
+        self.before_start()
+
+        self.running = True
+        while self.running:
+            self.game_loop()
+
+        pygame.quit()
+
+    def before_start(self):
+        self.left = 590
+        self.color = RED
+        self.count = 0
+
     """
     Handles a game update with the number of read and unread
     emails since the last time the GameManager was polled
@@ -35,39 +44,38 @@ class Game:
     def update(self, read: int, unread: int):
         pass
 
-    #main loop for the game
-    def gameLoop():
-        left = 590
-        color = red
-        count = 0
+    def draw_text(self, font_name: str, size: int, text: str, x: int, y: int, color: tuple):
+        font = pygame.font.Font(font_name, size)
+        surface = font.render(text, True, color)
+        self.display.blit(surface, (x, y))
 
-        gameNotClose = True
-        display.fill(black)
-        pygame.draw.rect(display, yellow, [90, 320, 10, 10])
-        yourScore(5)
-        pygame.display.update() #updates the screen
+    def draw_rect(self, color: tuple, x: int, y: int, length: int = 10, width: int = 10):
+        pygame.draw.rect(self.display, color, (x, y, length, width))
 
-        while gameNotClose == True:
-            background(left, color)
-            time.sleep(.05)
-            left -= 20
-            #restarts the background
-            if left <= 0:
-                count += 1
-                if count == 4:
-                     count = 1
-                left = 590
-                if count == 1:
-                    color = blue
-                elif count == 2:
-                    color = green
-                else:
-                    color = red
+    # main loop for the game
+    def game_loop(self):
+        self.display.fill(BLACK)
+        
+        self.draw_text("src/Treasuremap.ttf", 30, "Score     0", 0, 0, YELLOW)
+        
+        self.draw_rect(YELLOW, 100, 200)
+        self.draw_rect(RED, self.left, 100)
+        self.left += 1
+        # restarts the background
 
-            pygame.display.update() #updates the screen
+        print(self.left)
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    gameNotClose = False
-        pygame.quit
-        quit()
+        if self.left > 600:
+            self.left = 0
+
+        pygame.display.update()  # updates the screen
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+        
+        time.sleep(0.01)
+
+if __name__ == '__main__':
+    game = Game()
+    game.start()
