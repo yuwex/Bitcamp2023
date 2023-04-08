d57e39c4488a5fbebcf31e92166076edb26d9422
yuwe <25470828+yuwex@users.noreply.github.com>
2023-04-08 18:18:03 -0400

    Add comments

--- src/game.py
+++ src/game.py
@@ -4,6 +4,8 @@ A class for handling game mechanics
 import pygame
 import time
 
+# Final variables
+
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 102)
 RED = (213, 50, 80)
@@ -24,15 +26,19 @@ class Game:
         self.start()
     
     def start(self):
+        # Call before_start before starting
         self.before_start()
 
+        # Set running to true and run until false
         self.running = True
         while self.running:
             self.game_loop()
 
+        # Quit
         pygame.quit()
 
     def before_start(self):
+        # Define all variables here
         self.left = 590
         self.color = RED
         self.count = 0
@@ -46,33 +52,43 @@ class Game:
         pass
 
     def draw_text(self, font_name: str, size: int, text: str, x: int, y: int, color: tuple):
+        # Create a new Font object
         font = pygame.font.Font(font_name, size)
+
+        # Create a new Surface Object
         surface = font.render(text, True, color)
+
+        # Display Surface object on the screen
         self.display.blit(surface, (x, y))
 
     def draw_rect(self, color: tuple, x: int, y: int, length: int = 10, width: int = 10):
+        # Draw rectangle object on the screen
         pygame.draw.rect(self.display, color, (x, y, length, width))
 
     # main loop for the game
     def game_loop(self):
         self.display.fill(BLACK)
         
+        # Draw text with score data
         self.draw_text("src/Treasuremap.ttf", 30, f"Score {self.score}", 0, 0, YELLOW)
         
-        self.draw_rect(YELLOW, 100, 200)
+        # Draw moving red rectangle
         self.draw_rect(RED, self.left, 100)
         self.left += 1
-        # restarts the background
 
+        # Loop red rectangle at end
         if self.left > 600:
             self.left = 0
 
-        pygame.display.update()  # updates the screen
+        # Update Screen
+        pygame.display.update() 
 
+        # Quit if red dot pressed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
         
+        # Wait 0.01 Seconds
         time.sleep(0.01)
 
 if __name__ == '__main__':
