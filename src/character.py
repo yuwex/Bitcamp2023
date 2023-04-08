536d44ca32b5a6b85c2df32db23ad120351d4baa
Brandon <bjenkins0829@gmail.com>
2023-04-08 11:51:06 -0400

    Update character.py

--- src/character.py
+++ src/character.py
@@ -1,9 +1,12 @@
+import email_watcher
+
 class Character:
     level: int
     name: str
     def announce_email(self):
         print("You have a new email!")
     def announce_unread(self):
+        numUnread = email_watcher.Watcher.get_unread_count()
         if (numUnread <= 0):
           print("You have x unread emails!")
         elif (numUnread == 1):
@@ -11,4 +14,7 @@ class Character:
         elif (numUnread > 1):
           print("You have " + numUnread +  " unread emails!")
     def get_score(self):
-    
+        #score increases when user interacts with email (read, delete, archive, reply, forward, etc.)
+        #for read emails
+        numRead = email_watcher.Watcher.update()
+        level += numRead
