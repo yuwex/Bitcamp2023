06a3384e9f1514668d336caa2db8e984b5e22bf5
yuwe <25470828+yuwex@users.noreply.github.com>
2023-04-08 15:53:32 -0400

    Refactor inventory to return strings/more pythonic

--- src/inventory.py
+++ src/inventory.py
@@ -3,26 +3,34 @@ from item import Item
 class Inventory:
 
     doubloons: int
+    items: list[Item]
 
     def __init__(self):
         self.doubloons = 0
    
-    def buy(self, item: Item):
-        if (item.owned == False):
+    def buy(self, item: Item) -> str:
+        if not item.owned:
             if (self.doubloons >= item.cost):
+                self.items.append(item)
                 item.owned = True
-                print ("Sold! You now own " + item.name + "!\n It costs " + item.costToUpgrade + " doubloonss to upgrade this item!")
-                points -= item.cost
+                self.doubloons -= item.cost
+
+                return f"Sold! You now own {item.name}!\n It costs {item.costToUpgrade} doubloonss to upgrade this item!"
+
             else:
-                print ("Arrrr!! You don't have enough doubloons for this item! \n You need " + item.cost - self.doubloons + " more doubloons!")
+                return f"Arrrr!! You don't have enough doubloons for this item!\nYou need {item.cost - self.doubloons} more doubloons!"
+                
         else:
-            print ("You already own this item sailor! Try and buy something else...")
-    def sell_back(self, item: Item):
-        if (item.owned == True):
+            return "You already own this item sailor! Try and buy something else..."
+        
+    def sell_back(self, item: Item) -> str:
+        if item.owned:
                 item.owned = False
                 print ("Sold! You parted with " + item.name + " in exchange for "  + item.cost * 0.75 + " doubloons!")
                 self.doubloons += (item.cost * 0.75)
+
+                return f"Sold! You parted with {item.name} in exchange for {item.cost * 0.75} doubloons!"
         else:
-            print ("You don't own this matey! Try and sell something you already own!")
+            return "You don't own this matey! Try and sell something you already own!"
 
 
