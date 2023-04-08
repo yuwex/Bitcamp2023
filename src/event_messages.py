6276c358e030c04f8ae3aed7e72bfa20aa8dba88
yuwe <25470828+yuwex@users.noreply.github.com>
2023-04-08 16:11:43 -0400

    Refactor code
    rewrite inventory to have messages from event_messages

--- src/event_messages.py
+++ src/event_messages.py
@@ -1,5 +1,6 @@
 import json
 import random
+from typing import Any
 
 class EventMessages:
     on_email: list[str]
@@ -8,16 +9,36 @@ class EventMessages:
     def __init__(self, file: str = "event_messages.json"):
         with open(file, "r") as f:
             data = json.loads(f.read())
-            self.on_email = data["on_email"]
-            self.on_read = data["on_read"]
-        
-    def get_email_response(self) -> str:
+            self.on_email: list[str] = data["on_email"]
+            self.on_read: list[str] = data["on_read"]
+            self.on_buy_success: list[str] = data["on_buy_success"]
+            self.on_buy_expensive: list[str] = data['on_buy_expensive']
+            self.on_buy_owned: list[str] = data['on_buy_owned']
+            self.on_sell_success: list[str] = data['on_sell_success']
+            
+    def email_response(self) -> str:
         return random.choice(self.on_email)
     
-    def get_read_response(self) -> str:
+    def read_response(self) -> str:
         return random.choice(self.on_read)
+    
+    def buy_success(self, item: str, upgrade_cost: int) -> str:
+        return random.choice(self.on_buy_success).format(item=item, upgradeCost=upgrade_cost)
+    
+    def buy_expensive(self, cost: int) -> str:
+        return random.choice(self.on_buy_expensive).format(cost=cost)
+
+    def buy_owned(self) -> str:
+        return random.choice(self.on_buy_owned)
+    
+    def sell_success(self, item: str, cost: int) -> str:
+        return random.choice(self.on_sell_success).format(item=item, cost=cost)
 
 if __name__ == "__main__":
     em = EventMessages("src/event_messages.json")
-    print(em.get_email_response())
-    print(em.get_read_response())
+    print(em.read_response())
+    print(em.email_response())
+    print(em.buy_success("Sword", 50))
+    print(em.buy_expensive(100))
+    print(em.buy_owned())
+    print(em.sell_success("Shield", 22))
--- src/inventory.py
+++ src/inventory.py
@@ -8,29 +8,26 @@ class Inventory:
     def __init__(self):
         self.doubloons = 0
    
-    def buy(self, item: Item) -> str:
+    def buy(self, item: Item) -> bool:
         if not item.owned:
             if (self.doubloons >= item.cost):
                 self.items.append(item)
                 item.owned = True
                 self.doubloons -= item.cost
 
-                return f"Sold! You now own {item.name}!\n It costs {item.costToUpgrade} doubloonss to upgrade this item!"
+                return "bought"
 
             else:
-                return f"Arrrr!! You don't have enough doubloons for this item!\nYou need {item.cost - self.doubloons} more doubloons!"
+                return "expensive"
                 
         else:
-            return "You already own this item sailor! Try and buy something else..."
+            return "owned"
         
     def sell_back(self, item: Item) -> str:
         if item.owned:
                 item.owned = False
-                print ("Sold! You parted with " + item.name + " in exchange for "  + item.cost * 0.75 + " doubloons!")
                 self.doubloons += (item.cost * 0.75)
 
-                return f"Sold! You parted with {item.name} in exchange for {item.cost * 0.75} doubloons!"
-        else:
-            return "You don't own this matey! Try and sell something you already own!"
+                return "sold"
 
 
