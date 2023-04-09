dcb2df8bbd50519f0452ecf580136aaa39e17d92
yuwe <25470828+yuwex@users.noreply.github.com>
2023-04-08 22:08:36 -0400

    Image
    Move badguy to be child of Image

--- src/badguy.py
+++ src/badguy.py
@@ -1,16 +1,9 @@
-import os
+from image import Image
 import pygame
-class Badguy:
+import os
 
-    def __init__(self, image_name):
-        self.imageBadGuy = pygame.image.load(os.path.join(image_name))
-        new_size = (self.imageBadGuy.get_width() * 9, self.imageBadGuy.get_height() * 9)
-        self.imageBadGuy = pygame.transform.scale(self.imageBadGuy, new_size)
-        self.x = 600
-        self.y = 250
+class Badguy(Image):
 
-
-    def draw(self, surface: pygame.Surface):
-        surface.blit(self.imageBadGuy, (self.x, self.y))
-
+    def __init__(self, image):
+        super().__init__(image, 600, 250, 9, 9)
 
