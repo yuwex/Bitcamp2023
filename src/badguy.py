a1af7f98b7ef8009855fc3b53137175bc23e01ca
yuwe <25470828+yuwex@users.noreply.github.com>
2023-04-09 00:43:43 -0400

    add offset to badguy

--- src/badguy.py
+++ src/badguy.py
@@ -1,9 +1,13 @@
 from image import Image
 import pygame
+import random
 import os
 
 class Badguy(Image):
 
-    def __init__(self, image):
-        super().__init__(image, 600, 250, 9, 9)
+    def __init__(self, image, x_offset: int = 0, y_offset: int = 0):
+        super().__init__(image, 600 + x_offset, 250 + y_offset, 9, 9)
+        self.random_offset = random.randint(1, 100)
+    
+
 
